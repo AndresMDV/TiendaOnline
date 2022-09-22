5e65a8fa2b71e816d16226ccd0e9c7537b067af8
Andres Medina Gomez <andalfmedinag@hotmail.com>
2022-09-21 20:50:40 -0500

    Fin de videos de panel admin- video 20

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from gestionPedidos.models import Clientes, Articulos, Pedidos
 
-# Register your models here.
+
+class ClientesAdmin(admin.ModelAdmin):
+	list_display=("nombre", "direccion", "telefono")
+	search_fields = ("nombre",)
+
+
+class ArticulosAdmin(admin.ModelAdmin):
+	list_display=("nombre", "seccion", "precio")
+	search_fields = ("nombre",)
+	list_filter=("seccion","precio",)
+
+class PedidosAdmin(admin.ModelAdmin):
+	list_display=("numero", "fecha", "entregado")
+	search_fields = ("numero",)
+	list_filter=("fecha", "entregado",)
+	date_hierarchy = "fecha"
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
+
